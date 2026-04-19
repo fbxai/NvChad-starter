abef08abb8c52ad4e9fc65d8089905ae7d4224a0
NVFB <186336021+fbxai@users.noreply.github.com>
2026-04-19 15:49:19 -0700

    Cleaner starter and sensible copy/paste

--- README.md
+++ README.md
@@ -4,6 +4,17 @@
 - So you just import its modules , like `require "nvchad.options" , require "nvchad.mappings"`
 - So you can delete the .git from this repo ( when you clone it locally ) or fork it :)
 
+# Install
+
+Clone the repo into your own config folder
+```bash
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+```
+* Run `:MasonInstallAll` and `:TSInstallAll` command after lazy.nvim finishes downloading plugins.
+* Delete the `.git` folder from nvim folder.
+* Learn customization of ui & base46 from `:h nvui`.
+
+
 # Credits
 
 1) Lazyvim starter https://github.com/LazyVim/starter as nvchad's starter was inspired by Lazyvim's . It made a lot of things easier!
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,35 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
+
+-- 1. Disable automatic clipboard sync (recommended)
+-- This prevents normal d/y/c from touching the system clipboard unless you explicitly use "+y
+vim.opt.clipboard = ""
+
+-- 2. Keymaps: Make delete/change use the black-hole register ("_) by default
+-- This is the most common and clean solution.
+
+local map = vim.keymap.set
+
+-- Delete without yanking (normal + visual)
+map({ "n", "x" }, "d", '"_d', { desc = "Delete without yanking" })
+map({ "n", "x" }, "D", '"_D', { desc = "Delete to end of line without yanking" })
+
+-- Change without yanking
+map({ "n", "x" }, "c", '"_c', { desc = "Change without yanking" })
+map({ "n", "x" }, "C", '"_C', { desc = "Change to end of line without yanking" })
+
+-- Delete single character without yanking
+map({ "n", "x" }, "x", '"_x', { desc = "Delete char without yanking" })
+map({ "n", "x" }, "X", '"_X', { desc = "Delete char before cursor without yanking" })
+
+-- Substitute (s/S) without yanking
+map({ "n", "x" }, "s", '"_s', { desc = "Substitute without yanking" })
+map({ "n", "x" }, "S", '"_S', { desc = "Substitute line without yanking" })
+
+-- Optional: Keep a "cut" (delete + yank) mapping with <leader>
+-- So you can still cut when you need it: <leader>d or <leader>c
+map({ "n", "x" }, "<leader>d", "d", { desc = "Cut (delete + yank)" })
+map({ "n", "x" }, "<leader>c", "c", { desc = "Change + yank" })
+map({ "n", "x" }, "<leader>x", "x", { desc = "Cut char" })
